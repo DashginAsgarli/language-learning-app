9ea85c46ca9833d2092f7eb36017f6c4450b3896
DashginAsgarli <dashqinasgarli@gmail.com>
2026-03-01 21:39:04 +0400

    feat: implement LangDictionary component with search  logic

--- src/page/layout/layout.jsx
+++ src/page/layout/layout.jsx
@@ -3,14 +3,16 @@ import LanguageFeatures from '../../components/LanguageFeatures'
 import TopLanguages from '../../components/TopLanguages'
 import LangLearn from '../../components/LangLearn'
 import DailyChallenge from '../../components/DailyChallenge'
+import SmartDictionary from '../../components/SmartDictionary'
 
 function Layout() {
     return (
         <>
-        <LanguageFeatures />
-        <TopLanguages />
-        <LangLearn />
-        <DailyChallenge />
+            <LanguageFeatures />
+            <TopLanguages />
+            <LangLearn />
+            <DailyChallenge />
+            <SmartDictionary />
 
         </>
     )
